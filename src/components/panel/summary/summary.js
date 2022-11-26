61d6651c8d0a9c555159c30b5bfa9625d7b6d2d6
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-26 23:45:09 +0300

    Responsive to be updated. Grid-template-areas to be used.

--- src/components/panel/summary/summary.js
+++ src/components/panel/summary/summary.js
@@ -1,18 +1,72 @@
+import { useState } from "react";
 
 
 
 
 const Summary = () => {
     const grid_col = 4
+    const [details, setDetails] = useState(false);
 
     return ( 
-        <div className="Summary-comp" style={{gridTemplateColumns: `repeat(${grid_col},1fr)`}}>
-            <div className="Summary-comp_cell">CELL</div>
-            <div className="Summary-comp_cell" id="two">CELL</div>
-            <div className="Summary-comp_cell" id="three">CELL</div>
-            <div className="Summary-comp_cell">CELL</div>
-        </div>
+        <>
+            <div className="Summary-comp">
+                <div className="Summary-comp_cell">
+                    <div style={{backgroundColor: "black", color: "orange", padding: "10px"}}>
+                        HELOO HOTEL
+                    </div>
+                </div>
+                <div className="Summary-comp_cell" id="two">CELL</div>
+                <div className="Summary-comp_cell" id="three">CELL</div>
+                <div className="Summary-comp_cell">CELL</div>
+
+                <div className="Summary-comp_details">
+                    <div className="Summary-comp_details_shell">
+                                Details go here...
+                    </div>
+                </div>
+                <div className="Summary-comp_cell">
+                    <div style={{backgroundColor: "black", color: "orange", padding: "10px"}}>
+                        HELOO HOTEL
+                    </div>
+                </div>
+                <div className="Summary-comp_cell" id="two">CELL</div>
+                <div className="Summary-comp_cell" id="three">CELL</div>
+                <div className="Summary-comp_cell">CELL</div>
+
+                <div className="Summary-comp_details">
+                    <div className="Summary-comp_details_shell">
+                                Details go here...
+                    </div>
+                </div>
+            </div>
+            <div className="Summary-comp">
+                <div className="Summary-comp_cell">
+                    <div style={{backgroundColor: "black", color: "orange", padding: "10px"}}>
+                        HELOO HOTEL
+                    </div>
+                </div>
+
+                <div className="Summary-comp_cell" id="two">CELL</div>
+                <div className="Summary-comp_cell" id="three">CELL</div>
+                <div className="Summary-comp_cell">CELL</div>
+                <div className="Summary-comp_cell">
+                    <div style={{backgroundColor: "black", color: "orange", padding: "10px"}}>
+                        HELOO HOTEL
+                    </div>
+                </div>
+
+                <div className="Summary-comp_cell" id="two">CELL</div>
+                <div className="Summary-comp_cell" id="three">CELL</div>
+                <div className="Summary-comp_cell">CELL</div>
+
+
+            </div>
+        </>
+        
      );
 }
  
 export default Summary;
+
+
+/* style={{gridTemplateColumns: `repeat(${grid_col},1fr)`}} */
