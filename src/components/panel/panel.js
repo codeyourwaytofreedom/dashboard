b676515fbd63b9d69e37e128feb6479548eb4e65
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-27 22:04:48 +0300

    new grid design in place for easier responsive. existing componenets will be included in this grid design.

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -12,9 +12,36 @@ const Panel = () => {
     return ( 
         <div className="Panel-comp" style={{backgroundImage:`url(${image_horizontal})`}}>
             <div className="bubbles">
-                <Summary/>
+                <div className="one">
+                    <div className="one_kernel">
+                        ONE
+                    </div>
+                </div>
+                <div className="two">
+                    <div className="two_kernel">
+                        TWO
+                    </div>
+                </div>
+                <div className="three">
+                    <div className="three_kernel">
+                        THREE
+                    </div>
+                </div>
+                <div className="four">
+                    <div className="four_kernel">
+                        FOUR
+                    </div>
+                </div>
+                <div className="five">
+                    <div className="five_kernel">
+                        FIVE
+                    </div>
+                </div>
+{/*                 <Summary/>
                 <Visitors/>
                 <Finance/>
+                <Visitors/>
+                <Summary/> */}
             </div>
             
 
