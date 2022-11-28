357865e386045880c582991dd7057874af50f8ff
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-28 19:43:05 +0300

    Top menu done. All bubbles and pop-up bubbles made responsive. Responsive design tested with all devices. Perfectly smooth.
    Next step: modal for details after bubble click.
    
    Keep going.
    
    Remember why you started.

--- src/components/panel/bubble/bubble.js
+++ src/components/panel/bubble/bubble.js
@@ -1,11 +1,9 @@
 import "./bubble.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPercent } from '@fortawesome/free-solid-svg-icons';
+
 
 
 const Bubble = ({title, title_detail, bubble_up}) => {
-     title = <span className="orange" id={bubble_up === true ? "up" : "down"}>Occupancy Rate </span>;
-     title_detail = <span className="turq">89 <FontAwesomeIcon icon={faPercent} beatFade/> </span>;
+
     return ( 
         <div className="Bubble_comp">
             {title}
--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -1,6 +1,7 @@
 import Finance from "../finance/finance";
 import Bubble from "./bubble/bubble";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLuggageCart, faMap, faMapMarked, faMoneyBill1Wave, faPeopleGroup, faPercent } from '@fortawesome/free-solid-svg-icons';
 
 const Panel = () => {
     const image_horizontal = require("./hotel.jpeg")
@@ -11,27 +12,42 @@ const Panel = () => {
             <div className="bubbles">
                 <div className="one">
                     <div className="one_kernel">
-                        <Finance/>
+                        <Bubble 
+                            title = {<span className="orange" id="up">Finance </span>}
+                            title_detail = {<span className="turq"><FontAwesomeIcon icon={faMoneyBill1Wave} size={"lg"} color={"lightgreen"} beatFade/> </span>}                        
+                        />
                    </div>
                 </div>
                 <div className="two">
                     <div className="two_kernel">
-                        <Bubble bubble_up={false}/>
+                        <Bubble 
+                            title = {<span className="orange" id="down">Visitors </span>}
+                            title_detail = {<span className="turq"><FontAwesomeIcon icon={faLuggageCart} size={"lg"} color={"salmon"} beatFade/> </span>}
+                        />
                     </div>
                 </div>
                 <div className="three">
                     <div className="three_kernel">
-                        <Bubble bubble_up={true}/>
+                        <Bubble 
+                            title = {<span className="orange" id="up">Occupancy Rate </span>}
+                            title_detail = {<span className="turq"><FontAwesomeIcon icon={faPercent}  size={"lg"} color={"red"} beatFade/> </span>}                        
+                        />
                    </div>
                 </div>
                 <div className="four">
                     <div className="four_kernel">
-                        FOUR
+                        <Bubble 
+                            title = {<span className="orange" id="down">Staff</span>}
+                            title_detail = {<span className="turq"><FontAwesomeIcon icon={faPeopleGroup} size={"lg"} color={"silver"} beatFade/> </span>}
+                        />
                     </div>
                 </div>
                 <div className="five">
                     <div className="five_kernel">
-                        <Bubble bubble_up={true}/>
+                        <Bubble 
+                            title = {<span className="orange" id="up">Facilities</span>}
+                            title_detail = {<span className="turq"><FontAwesomeIcon icon={faMapMarked} size={"lg"} color={"silver"} beatFade/> </span>}                        
+                        />
                     </div>
                 </div>
             </div>
