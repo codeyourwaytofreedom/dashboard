5a97ec842b1169fcfb2483a7d997dc3d4f74cdff
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-29 16:27:37 +0300

    Details component inner cells complete. Props in place. Cell component receives html elements as props.
    Next step: Create detail cells for second bubble.
    
    Keep going.
    
    Remember why you started.

--- src/components/details/cell.js
+++ src/components/details/cell.js
@@ -2,16 +2,11 @@
 
 
 
-const Cell = () => {
+const Cell = ({title, content}) => {
     return ( 
         <div className="details_kernel_cell">
-                        <div className='details_kernel_cell_title'><span style={{color:"darkgreen"}}>Turnover</span></div>
-                        <div className='details_kernel_cell_content'>
-                            <div>
-                                <span>Jan-Jun:  §765.000</span><br />
-                                <span>Jun-Sep:  §341.000</span>
-                            </div>
-                        </div>
+                        <div className='details_kernel_cell_title'>{title}</div>
+                        <div className='details_kernel_cell_content'>{content}</div>
                     </div>
      );
 }
--- src/components/details/details.js
+++ src/components/details/details.js
@@ -1,23 +1,54 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons';
-
+import { Chart } from "react-google-charts";
 
 import "./details.css";
 import Cell from './cell';
 const Details = () => {
+     const data = [
+        ["Element", "Density", { role: "style" }],
+        ["Copper", 8.94, "#b87333"], // RGB value
+        ["Silver", 10.49, "silver"], // English color name
+      ];
     return ( 
         <div className="details"> 
             <div className="details_kernel">
-                    <div className="details_kernel_cell">
-                        <div className='details_kernel_cell_title'><span style={{color:"darkgreen"}}>Turnover</span></div>
-                        <div className='details_kernel_cell_content'>
+
+                    <Cell 
+                        title={<span style={{color:"darkgreen"}}>Turnover</span>}
+                        content={
                             <div>
+                                <span style={{color:"crimson", fontSize:"21px"}}>2022</span><br />
                                 <span>Jan-Jun:  §765.000</span><br />
                                 <span>Jun-Sep:  §341.000</span>
                             </div>
-                        </div>
-                    </div>
-                    <Cell/>
+                        }
+                    />
+
+
+
+
+                    <Cell 
+                        title={<span style={{color:"crimson"}}>Last 3 Years</span>}
+                        content={
+                                    <div style={{display:"grid", alignItems:"end", height:"112px", 
+                                         width:"150px"}}>
+                                        <span style={{width:"45%", height:"30px", backgroundColor:"blue", 
+                                            textAlign:"start", color:"white", paddingLeft:"10px", opacity:"1", borderBottomRightRadius:"10px"}}>
+                                        2020
+                                        </span>
+                                        <span style={{width:"95%", height:"30px", backgroundColor:"crimson",
+                                            textAlign:"start", color:"white", paddingLeft:"10px", opacity:"1", borderBottomRightRadius:"10px"}}>2021</span>
+                                        <span style={{width:"75%", height:"30px", backgroundColor:"black",
+                                            textAlign:"start", color:"white", paddingLeft:"10px", opacity:"1", borderBottomRightRadius:"10px"}}>2022</span>
+                                    </div>
+                            
+                        }
+                    />
+
+
+
+
                     <div className="details_kernel_cell">
                         <div className='details_kernel_cell_title'>Quick Calls</div>
                         <div className='details_kernel_cell_content'>
