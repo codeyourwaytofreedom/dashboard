c2a1557b33b286c411704bd14622df6eebd2b050
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-29 14:13:46 +0300

    Details component 70% done. Cell components created. Existing code is being component-ized to allow... customisation.
    Great Progress today. Things look smooth.
    
    Keep going.
    
    Remember why you started.

---
+++ src/components/details/cell.js
@@ -0,0 +1,19 @@
+
+
+
+
+const Cell = () => {
+    return ( 
+        <div className="details_kernel_cell">
+                        <div className='details_kernel_cell_title'><span style={{color:"darkgreen"}}>Turnover</span></div>
+                        <div className='details_kernel_cell_content'>
+                            <div>
+                                <span>Jan-Jun:  §765.000</span><br />
+                                <span>Jun-Sep:  §341.000</span>
+                            </div>
+                        </div>
+                    </div>
+     );
+}
+ 
+export default Cell;
--- src/components/details/details.js
+++ src/components/details/details.js
@@ -3,21 +3,40 @@ import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons';
 
 
 import "./details.css";
+import Cell from './cell';
 const Details = () => {
     return ( 
         <div className="details"> 
             <div className="details_kernel">
-                    <div>One</div>
+                    <div className="details_kernel_cell">
+                        <div className='details_kernel_cell_title'><span style={{color:"darkgreen"}}>Turnover</span></div>
+                        <div className='details_kernel_cell_content'>
+                            <div>
+                                <span>Jan-Jun:  §765.000</span><br />
+                                <span>Jun-Sep:  §341.000</span>
+                            </div>
+                        </div>
+                    </div>
+                    <Cell/>
+                    <div className="details_kernel_cell">
+                        <div className='details_kernel_cell_title'>Quick Calls</div>
+                        <div className='details_kernel_cell_content'>
+                            <span id='icon'>
+                                <FontAwesomeIcon icon={faPhoneFlip} rotation={270} size={"xl"} color={"#ff7b25"}/>
+                            </span>
+                            <div id='number'>
+                                <span>+(90)-553-458-37-41</span>
+                                <span style={{color:"#ff7b25"}}>Chief of department</span>
+                            </div>
+                        </div>
+                    </div>
+
+
                     <div>Two</div>
                     <div>Three</div>
                     <div>Four</div>
                     <div>Five</div>
-                    <div className='quick_calls'>
-                        <div className='quick_calls_text'>Quick Calls</div>
-                        <div className='quick_calls_icon'>
-                            <FontAwesomeIcon icon={faPhoneFlip} rotation={270} size={"2x"}/>
-                        </div>
-                    </div>
+                    
             </div>
         </div>
      );
