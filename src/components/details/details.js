dfe32501022bc280196d3901a4278b280a3ba335
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-29 12:42:02 +0300

    Details component made responsive. Details component inner content under construction.
    Remember why you started.

--- src/components/details/details.js
+++ src/components/details/details.js
@@ -1,9 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhoneFlip } from '@fortawesome/free-solid-svg-icons';
+
+
 import "./details.css";
 const Details = () => {
     return ( 
         <div className="details"> 
             <div className="details_kernel">
-                    Details go here....
+                    <div>One</div>
+                    <div>Two</div>
+                    <div>Three</div>
+                    <div>Four</div>
+                    <div>Five</div>
+                    <div className='quick_calls'>
+                        <div className='quick_calls_text'>Quick Calls</div>
+                        <div className='quick_calls_icon'>
+                            <FontAwesomeIcon icon={faPhoneFlip} rotation={270} size={"2x"}/>
+                        </div>
+                    </div>
             </div>
         </div>
      );
