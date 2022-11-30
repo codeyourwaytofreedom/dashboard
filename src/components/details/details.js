6cb01d46c35b806da065fd202b09da2bb1ff1f33
codeyourwaytofreedom <can.kolej@gmail.com>
2022-11-30 13:00:59 +0300

    Gauges design made mobile responsive.
    Final component to be made mobile responsive: Finance.
    
    Keep going.
    
    Remember why you started.

--- src/components/details/details.js
+++ src/components/details/details.js
@@ -64,21 +64,21 @@ const Details = ({details}) => {
                         <div className='gauge' style={{backgroundImage:`url(${gag})`}}>
                             <img src={require("./needle.png")} alt="needle" id='gauge1' />
                             <div className='data'>
-                                    <span style={{color:"crimson"}}>Russian</span>
-                                    <span style={{color:"yellow"}}>7.865</span>                                
+                                    <span className='span_top' style={{color:"crimson"}}>Russian</span>
+                                    <span className='span_bottom' style={{color:"yellow"}}>7.865</span>                                
                             </div>
                         </div>
                         <div className='gauge' style={{backgroundImage:`url(${gag})`}}>
                             <img src={require("./needle.png")} alt="needle" id='gauge2' />
                             <div className='data'>
-                                    <span style={{color:"crimson"}}>Dutch</span>
+                                    <span className='span_top' style={{color:"crimson"}}>Dutch</span>
                                     <span style={{color:"yellow"}}>7.312</span>                                
                             </div>
                         </div>
                         <div className='gauge' style={{backgroundImage:`url(${gag})`}}>
                             <img src={require("./needle.png")} alt="needle" id='gauge3' />
                             <div className='data'>
-                                    <span style={{color:"crimson"}}>British</span>
+                                    <span className='span_top' style={{color:"crimson"}}>British</span>
                                     <span style={{color:"yellow"}}>6.990</span>                                
                             </div>
                         </div>
